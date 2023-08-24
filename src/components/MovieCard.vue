1bb09786a8f3b5955e46295cc5f03b79ff36027e
DDS58 <118191687+DDS58@users.noreply.github.com>
2023-08-24 19:40:32 -0400

    display loop card

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="movie-card">
+    <img :src="posterUrl" :alt="title" class="movie-poster" />
+    <h3 class="movie-title">{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    posterUrl: String,
+  },
+};
+</script>
