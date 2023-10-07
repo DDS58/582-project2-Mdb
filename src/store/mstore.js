592b833f427b2b5f7782bd8788a4a35d9c2e22f1
DDS58 <118191687+DDS58@users.noreply.github.com>
2023-10-06 20:45:47 -0400

    publicPath, with codespace link

--- src/store/mstore.js
+++ src/store/mstore.js
@@ -3,8 +3,7 @@ import { defineStore } from "pinia";
 export const useMoviestore = defineStore("mstore", {
   state: () => ({
     // to run index.js on codespaces then use this url
-    workingUrl:
-      "https://vigilant-space-winner-6696pwpw6rr2477v-3000.app.github.dev/",
+    workingUrl: "https://urban-broccoli-r949xwxwwxq3x9qj-3000.app.github.dev/",
     // using at home without codespaces for faster dev
     // workingUrl: "http://localhost:3000/",
   }),
